9e8eccd63a5394979524ff8d853c7b0dc9f0a967
Trevor Seitz <trevinla@gmail.com>
2018-02-20 15:51:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var index = 0;
+      key = parseInt(e.detail || e.which);
 
+  function onKeyDownHandler(e) {
+    var key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        console.log('Hooray!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
