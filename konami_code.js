1b100f5215b6ed6f3b4eea4d8eda4a9d422a803b
Trevor Seitz <trevinla@gmail.com>
2018-02-20 16:12:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,7 +9,7 @@ function init() {
 
     if (key === code[index]) {
       index++;
-      if (index === code.length-1) {
+      if (index === code.length - 3) {
         window.alert('Hooray!');
         index = 0;
       }
